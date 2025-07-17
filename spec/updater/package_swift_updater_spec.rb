ad4504c0ad0fe2755c0e458cb76b6b694f28fe86
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2025-07-17 15:22:23 -0300

    Add more test cases

--- spec/updater/package_swift_updater_spec.rb
+++ spec/updater/package_swift_updater_spec.rb
@@ -39,4 +39,39 @@ describe DependenciesSynchronizer::Updater::PackageSwiftUpdater do
       expect(updated).to include("exact: \"1.1.5\"")
     end
   end
+
+  it 'updates from version of dependencies in Package.swift while preserving from:' do
+    content_with_from = <<~SWIFT
+      .package(url: "https://github.com/YourOrg/org-ml1-dep-ios-sanetwork.git", from: "1.1.5")
+      .product(name: "SANetwork", package: "org-ml1-dep-ios-sanetwork")
+    SWIFT
+
+    allow(store).to receive(:version_for).with('SANetwork').and_return('2.5.0')
+
+    with_temp_package_swift(content_with_from) do |path|
+      described_class.new(store).update(path)
+      updated = File.read(path)
+
+      expect(updated).to include('from: "2.5.0"')
+      expect(updated).to_not include('exact: "2.5.0"')  # Ensures from: was preserved
+    end
+  end
+
+  it 'does not alter package entries with unknown constraint types' do
+    content_with_branch = <<~SWIFT
+      .package(url: "https://github.com/YourOrg/org-ml1-dep-ios-saanalytics.git", branch: "develop")
+      .product(name: "SAAnalytics", package: "org-ml1-dep-ios-saanalytics")
+    SWIFT
+
+    allow(store).to receive(:version_for).with('SAAnalytics').and_return('2.0.0')
+
+    with_temp_package_swift(content_with_branch) do |path|
+      original_content = File.read(path)
+      described_class.new(store).update(path)
+      updated_content = File.read(path)
+
+      expect(updated_content).to eq(original_content) # Unchanged
+    end
+  end
 end
+
